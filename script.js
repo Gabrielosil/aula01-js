3416137e7391464369716b99cc3102efda2241c8
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-08-21 20:03:30 -0300

    Feat: Notas do alunos e mensagem de media

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-//exibir no terminal
-console.log("Olá Mundo");
-//exibir no Navegador
-document.write("Olá Navegador");
-//um alerta
-alert("Alerta de Confirmação!");
+/*
+Faça um algoritimo onde o usuario informe o nome
+ e 4 notas de dois alunos e mostre as médias desses 2 alunos
+*/
 
-//string
+var nome = window.prompt("Informe o seu nome");
+
+var nota1 = window.prompt("Digite a nota 1:");
+nota1 = parseFloat(nota1)
+
+var nota2 = window.prompt("Digite a nota 2:");
+nota2 = parseFloat(nota2)
+
+var nota3 = window.prompt("Digite a nota 3:");
+nota3 = parseFloat(nota3)
+
+var nota4 = window.prompt("Digite a nota 4:");
+nota4 = parseFloat(nota4)
+
+var media = (nota1 + nota2 + nota3 + nota4)/4;
 
 
-//Capturar o nome do Aluno
-var nome = window.prompt("informe o Nome do Aluno:");
-// Guardar a primeira nota
-var nota1 = window.prompt("Digite a Nota 1");
-//converte a nota1 de texto para decimal
-nota1 = parseFloat (nota1);
-// Guardar a segunda nota
-var nota2 = window.prompt("Digite a Nota 2");
-//converte a nota2 de texto para decimal
-nota2 = parseFloat (nota2);
-//calculo a média das duas notas
-var media = (nota1 + nota2)/2;
-//Gero a mensagem de exibição do nome e da média do aluno
-var mensagem = nome + ", você obteve a média: " + media;
+var nome1 = window.prompt("Informe o seu nome");
+
+var nota10 = window.prompt("Digite a nota 1:");
+nota10 = parseFloat(nota10)
+
+var nota20 = window.prompt("Digite a nota 2:");
+nota20 = parseFloat(nota20)
+
+var nota30 = window.prompt("Digite a nota 3:");
+nota30 = parseFloat(nota30)
+
+var nota40= window.prompt("Digite a nota 4:");
+nota40 = parseFloat(nota40)
+
+var media1 = (nota10 + nota20 + nota30 + nota40)/4;
+
+
+var mensagem = nome + ", Você Obteve a Media: " + media + " e " + nome1 + ", Você Obteve a Media: " + media1 ;
 window.alert(mensagem);
